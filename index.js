f780a85cfc6891306a08ed9d1d32c8c453e13bf8
Javier Minchala <javierminchala@Javiers-MacBook-Air.local>
2019-05-31 14:26:29 -0400

    Fixed bugs with board-resizing

--- index.js
+++ index.js
@@ -1,34 +1,82 @@
 
 let num_rows = 0;
-let num_cols = 1;
+let num_cols = 0;
 
 let rootElID = 'pixel-board';
 let rootElement = document.getElementById(rootElID);
 
+let selectedColor = "red";
+
+let allowFreeDraw = false;
+
+rootElement.onmousedown = function () {
+
+	
+
+}
+
+let makeClickable = function (el) {
+
+	el.props = {
+		clicked: false
+	};
+
+	el.onclick = function () {
+
+		if (el.props.clicked == false) {
+			el.style.backgroundColor = selectedColor;
+		}
+
+		else {
+			el.style.backgroundColor = "transparent";
+		}
+
+		el.props.clicked = !el.props.clicked;
+	}
+
+	el.onmouseover = function () {
+
+		if (allowFreeDraw == false) {
+
+			return;
+
+		}
+
+		if (el.props.clicked == false) {
+
+			el.style.backgroundColor = selectedColor;
+			el.props.clicked = true;
+
+		}
+	}
+
+}
+
 let addRows = function () {
 
-		if (num_cols == 0) {
+	let r = document.createElement('tr');
 
-			num_cols++;
+	for (let i = 0; i < num_cols; i++) {
 
-		}
+		let c = document.createElement('td');
 
-		let r = document.createElement('tr');
+		makeClickable(c);
 
-		for (let i = 0; i < num_cols; i++) {
+		r.appendChild(c);
 
-			let c = document.createElement('td');
+	}
 
-			r.appendChild(c);
+	num_rows++;
 
-		}
-
-		rootElement.appendChild(r);
-		num_rows++;
+	rootElement.appendChild(r);
 
 }
 
 let removeRows = function () {
+
+	if (num_rows <= 0) {
+		return;
+	}
 
 	let rows = document.querySelectorAll('#' + rootElID + ' tr');
 	let lastRow = rows[rows.length - 1];
@@ -37,7 +85,7 @@ let removeRows = function () {
 
 	num_rows--;
 
-	if (num_rows == 0) {
+	if (num_rows <= 0) {
 
 		num_cols = 0;
 
@@ -48,17 +96,14 @@ let removeRows = function () {
 
 let addCols = function () {
 
-	if (num_rows == 0) {
-
-		num_rows++;
-
-	}
-
 	let rows = document.querySelectorAll('#' + rootElID +' tr');
 
 	for (let i = 0; i < rows.length; i++) {
 
 		let c = document.createElement('td');
+		
+		makeClickable(c);
+
 		rows[i].appendChild(c);
 
 	}
@@ -68,6 +113,10 @@ let addCols = function () {
 }
 
 let removeCols = function () {
+
+	if (num_cols <= 0) {
+		return;
+	}
 
 	let rows = document.querySelectorAll('#' + rootElID + ' tr');
 
@@ -80,26 +129,50 @@ let removeCols = function () {
 
 	num_cols--;
 
-	if (num_cols == 0) {
+	if (num_cols <= 0) {
 
 		num_rows = 0;
 
+		rootElement.innerHTML = "";
 	}
 }
 
 document.getElementById('add-rows').onclick = function () { 
-	addRows() 
+
+	if (num_cols == 0) {
+		num_cols = 1;
+	}
+
+	addRows();
+
+
+	console.log(num_rows + ", " + num_cols);
+
 };
 
 document.getElementById('remove-rows').onclick = function () {
 	removeRows();
+
+
+	console.log(num_rows + ", " + num_cols);
 }
 
 
 document.getElementById('add-cols').onclick = function () {
+	
+	if (num_cols == 0) {
+		addRows();
+	}
+
 	addCols();
+
+
+	console.log(num_rows + ", " + num_cols);
 }
 
 document.getElementById('remove-cols').onclick = function () {
 	removeCols();
+
+
+	console.log(num_rows + ", " + num_cols);
 }
